9cc5578d89779cb7ab8a5e39a0e2669812c94127
TENMAJKL <silder.hologram@gmail.com>
2025-02-06 10:44:23 +0000

    adds basic function

--- ocarina/ocarina.ino
+++ ocarina/ocarina.ino
@@ -1,13 +1,66 @@
 #include<Wire.h>
 #include <Adafruit_BMP085.h>
 
+#define POT_PIN 26
+#define A44 69
+#define FIRST_PIN 6
+
 Adafruit_BMP085 bmp;
 
 int blow_low = 99800;
 int blow_high = 101000;
 
+float baseline_velocity = 0;
+float velocity_range = 0;
 
-void setup() {
+const int scales[8] = {
+    {0, 2, 4, 5, 7, 9, 11, 12}, // major
+    {0, 2, 3, 5, 7, 8, 10, 12}, // minor
+};
+
+
+int base = A44;
+int scale = 0;
+int channel = 5;
+int control = 1;
+
+// todo add some setting mode
+
+/**
+ * @author Oliver Torr
+ * todo led indications
+ */
+void calibrate_bmp() 
+{
+    float readings[50];
+    int index = 0;
+    unsigned long start_time = millis();
+
+    while (millis() - start_time < 10000) {
+        if (index < 50) {
+            readings[index++] = bmp.readPressure();
+        }
+        delay(100);  
+    }
+
+    float sum = 0;
+    for (int i = 0; i < index; i++) {
+        sum += readings[i];
+    }
+    baseline_velocity = (index > 0) ? sum / index : bmp.readPressure();
+
+    float max_deviation = 0;
+    for (int i = 0; i < index; i++) {
+        float deviation = abs(readings[i] - baseline_velocity);
+        if (deviation > max_deviation) {
+            max_deviation = deviation;
+        }
+    }
+    velocity_range = max_deviation;
+}
+
+void setup() 
+{
   Serial.begin(9600);
   Serial1.begin(31250);
   bmp.begin();
@@ -15,34 +68,51 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
-//int last = 0;
-//int current = 0;
+int last = 0;
 
 int note = 0;
+int last_velocity = 0;
+int last_pot = 0;
 
-void loop() {
-//    for (int i = 0; i < 4; i++) {
-//        if (digitalRead(i) == HIGH) {
-//            current |= 1 << i;
-//        }
-//    }
-//
-//    if (current != last) { // && foukam
-//         // send midi note
-//    }
-//
-//    // send foukani
+void loop() 
+{
+    int current = 0;
+    for (int i = 0; i < 4; i++) {
+        if (digitalRead(FIRST_PIN + i) == HIGH) {
+            current |= 1 << i;
+        }
+    }
+    
+    float pressure = bmp.readPressure();
+    int velocity = max(0, min(127, int(((pressure - baseline_pressure) / pressure_range) * 127))); // todo rewrite 
 
-//Serial1.write(1);
-  Serial.println(bmp.readPressure());
-  if (bmp.readPressure() < blow_low) {
-    return;
-  }
-  Serial1.write(0b10010100);
-  Serial1.write(69);
-  Serial1.write(map(bmp.readPressure(), blow_low, blow_high, 1, 127));
-  //digitalWrite(LED_BUILTIN, HIGH);
-  //delay(100);
-  //digitalWrite(LED_BUILTIN, LOW);
-  //delay(400); 
+    if (current != last || velocity != last_velocity) {
+        last = current;
+        last_velocity = velocity;
+        noteOn(
+            channel, 
+            base + scales[scale][last], 
+            velocity);
+    }
+
+    int pot = analogRead(POT_PIN);
+    if (pot != last_pot) {
+        cc(channel, control, map(pot, 0, 1023, 0, 127));
+    }
+
+
+}
+
+void noteOn(int channel, int note, int velocity)
+{
+  Serial1.write(0b10010000 | channel);
+  Serial1.write(note);
+  Serial1.write(velocity);
+}
+
+void cc(int channel, int control, int value)
+{
+  Serial1.write(0b10110000 | channel);
+  Serial1.write(control);
+  Serial1.write(value);
 }
